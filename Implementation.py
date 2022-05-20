4bbac8b18530d52e280b37627b87f3a8683fd2d2
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2022-05-20 21:45:18 +0530

    Update Implementation.py

--- Implementation.py
+++ Implementation.py
@@ -55,38 +55,12 @@ for i in actions: #Iterate through number of actions
         try: 
             os.makedirs(os.path.join(DATA_PATH, i, str(j))) #Store them in folders
         except: 
-            print('Error creating folder in ', i, j, 'iteration') #Check whether the folder already exists, if yes then pass on
+            #print('Error creating folder in ', i, j, 'iteration') #Check whether the folder already exists, if yes then pass on
             pass 
-
-#This Cell is for Collection of the Data through CV2 and then saving them to a Folder and labelling them
-cam = cv2.VideoCapture(0)
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic: # Set mediapipe model
-    for action in actions: #Iterate through actions
-        for sequence in range(no_sequences): #Iterate through no. of videos
-            for frame_num in range(sequence_length): #Iterate through no. of frames
-                ret, frame = cam.read() #Collect Frame
-                image, results = mp_detect(frame, holistic) #Detect the hand coordinates
-                draw_styled_landmarks(image, results) # Draw landmarks
-                
-                #Applying a logic to wait for collection of data
-                if frame_num == 0: #For initially starting collection of a particular action
-                    cv2.putText(image, 'Collecting Data for {} Video Number {}'.format(action, sequence), (150,25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (242, 48, 103), 1, cv2.LINE_AA)
-                    cv2.imshow('DATA COLLECTION WINDOW', image) # Display the resulting frame
-                    cv2.waitKey(1)
-                else: 
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (150,25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (242, 48, 103), 1, cv2.LINE_AA)
-                    cv2.imshow('DATA COLLECTION WINDOW', image) # Display the resulting frame
-                keypoints = Hand_points(results) #Call function Hand_points and store the result
-                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num)) #Decide the folder to save the frame or result
-                np.save(npy_path, keypoints) #Save the result in the given folder path
-                if cv2.waitKey(10) & 0xFF == ord('a'): #Wait time between data collection and press 'a' to skip that data for that iteration
-                    break  #Back to new data collection for the same action                 
-    cam.release() #Turn off the camera
-    cv2.destroyAllWindows() #Turn of all the CV2 Windows
-    
+        
 #Label the actions starting from 0 to n
 label_map = {label:num for num, label in enumerate(actions)}
-print(label_map) #Print the label    
+#print(label_map) #Print the label 
 
 #This Cell is for Appending or storing all the data in a single array
 sqs, lbls = [], [] #Create empty list 
@@ -99,48 +73,6 @@ for i in actions: #Iterate thorugh actions
         sqs.append(temp) #Save those frames in a list 
         lbls.append(label_map[i]) #Save the list of an action to label list, for eg. hello, ty, etc.
         
-#This cell is for Splitting the DATA and defining input and target for the model, later printing the shape of it respectively
-#X is the attributes and Y is the target and then we split 50% of the data into train and testing 
-X = np.array(sqs) #Attributes
-y = to_categorical(lbls).astype(int) #Target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5) #Splitting
-print('Number of Data:', len(X), 'and Number of Target :', len(y))
-print('Train Data :', X_train.shape,' and Train Target :',y_train.shape)
-print('Test Data :', X_test.shape,' and Test Target :',y_test.shape) #126 is the number of coordinates of the hand
-
-log_dir = os.path.join('Logs') #Create Logs for the model
-tb_callback = TensorBoard(log_dir=log_dir) #used for analyzing the Data flow graph and used to understand machine-learning models.
-#Create LSTM Model and train it
-#Sequential model is a linear stack of layers
-model = Sequential() #A Sequential model is appropriate for a plain stack of layers where each layer has exactly one input tensor and one output tensor.
-model.add(LSTM(64, return_sequences=True, activation='tanh', input_shape=(30,126))) #126 is the number of coordinates of the hand
-model.add(LSTM(128, return_sequences=True, activation='tanh'))
-model.add(LSTM(64, return_sequences=False, activation='tanh'))
-model.add(Dense(64, activation='tanh')) #Generally ReLu Activation Function is preffered over 
-model.add(Dense(32, activation='tanh')) 
-model.add(Dense(actions.shape[0], activation='softmax')) #softmax as an activation function is helpful for amplifying error gradients
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy']) #Configures the model for training.
-model.fit(X_train, y_train, epochs=500, callbacks=[tb_callback]) #Train the model
-
-model.summary() #Print the summary of the trained model
-
-#Check whether the Y_test and Predicted Target have the same class
-res = model.predict(X_test) #Check the result for X_test data predicted in the model
-print('Predicted Result :', actions[np.argmax(res[100])]) #Print the nth term of the predicted result
-print('Actual Result :',actions[np.argmax(y_test[100])]) #Print the nth term of Y_test, if both are same, model is trained properly
-
-model.save('Final_Submission_2_50.h5') #Save our model
-
-import seaborn as sns #Import for Heatmap
-y_estimated = np.argmax(model.predict(X_test), axis=1).tolist() #Estimated Values for X_test Data which is Y_Test predicted
-y_actual = np.argmax(y_test, axis=1).tolist() #Actual Values of Y_test which is Y_Test actual
-cm = multilabel_confusion_matrix(y_actual, y_estimated) #Confusion Multilabled Matrix
-for i in range(len(cm)) : #Iterate through Confusion Matrix of each Label
-    print('For Gesture -', actions[i], ':\n',cm[i]) #Print Confusion Matrix for each label
-    plt.figure(i) #Define a plot for Heamap
-    ax = sns.heatmap(cm[i], cmap="binary", center=0, annot=True, linewidths=.5) #Print all the Heatmaps Orderwise
-print('\nTotal Accuracy :',accuracy_score(y_estimated, y_actual)) #Print the Accuracy of the Model
-
 #7 
 model = load_model('Final_Submission_2_50.h5') #Call our model
 colors = [(170,60,150), (117,245,16), (19,69,139), (255,0,0), (255,0,255), (16,117,245), (0,215,255), (0,0,255),  (185,218,255)] #BGR Colour Palletes for the Predicted Word
@@ -154,7 +86,7 @@ def probability_colours(res, actions, input_frame, colors): #Function to check t
 
 sequence = [] #Empty List for Sequence - Number of frames in a video
 sentence = [] #Empty List for all the Sentences
-threshold = 0.8 #Threshold value for Prediction Result
+threshold = 0.9 #Threshold value for Prediction Result
 cam = cv2.VideoCapture(0) #Turn on the Camera
 with mp_holistic.Holistic(min_detection_confidence=0.9, min_tracking_confidence=0.9) as holistic: #Tracking 
     while cam.isOpened(): #Camera is opened
